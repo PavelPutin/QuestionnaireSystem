863ada599d34f6ecf52b6e27f4af904e8d427f2c
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-09-26 20:02:54 +0300

    feat: create project with spring initializer

---
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/QuestionnaireSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.vsu.putinpa.questionnairesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnaireSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/vsu/putinpa/questionnairesystem/QuestionnaireSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vsu.putinpa.questionnairesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
