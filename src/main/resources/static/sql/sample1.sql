98209478123f5a64f45c6e98122835e8c09f8fcc
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-09-26 20:38:53 +0300

    feat: add data to db

--- src/main/resources/static/sql/sample1.sql
+++ src/main/resources/static/sql/sample1.sql
@@ -2,5 +2,5 @@
 select q.name, opt.text, count(o_c.choice_id) from questionnaire q 
 	left join option opt on q.id=opt.questionnaire_id
 	left join option_choice o_c on o_c.option_id=opt.id
-	where name='Граница обучения кадров сделала своё дело'
+	where name='Как бы то ни было, убеждённость некоторых оппонентов расставила все точки над i'
 	group by q.name, opt.text;
