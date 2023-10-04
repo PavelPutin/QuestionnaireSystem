092c3ed1e0323b356426c5bea3db2b796be2abc6
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-10-04 21:26:38 +0300

    feat: create principal repository

--- src/main/java/edu/vsu/putinpa/questionnairesystem/model/Principal.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/model/Principal.java
@@ -2,6 +2,7 @@ package edu.vsu.putinpa.questionnairesystem.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,4 +13,8 @@ public class Principal {
     @Id
     private String username;
     private String password;
+    @OneToOne(mappedBy = "principal")
+    private Author author;
+    @OneToOne(mappedBy = "principal")
+    private Interviewee interviewee;
 }
