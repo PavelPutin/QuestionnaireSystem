727c6799e34580bd155b54b5d9594eacbfc1b690
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-10-24 15:26:20 +0300

    feat: add questionnairy get all method to permit all security config

--- src/main/java/edu/vsu/putinpa/questionnairesystem/config/SecurityConfiguration.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/config/SecurityConfiguration.java
@@ -19,7 +19,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/registration/**", "/auth/login").permitAll()
+                        .requestMatchers("/auth/registration/**", "/auth/login", "/questionnaire").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(login -> login
                         .loginProcessingUrl("/auth/login")
