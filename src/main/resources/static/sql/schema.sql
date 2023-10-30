4f884d16422751a48f0189163b5bb85a4caa10af
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-10-30 22:37:00 +0300

    feat: update schema to V2

--- src/main/resources/static/sql/schema.sql
+++ src/main/resources/static/sql/schema.sql
@@ -1,59 +1,70 @@
-CREATE TABLE IF NOT EXISTS principal (
-    username VARCHAR(100) PRIMARY KEY,
-    password VARCHAR(40) NOT NULL
-    );
+create table if not exists country
+(
+    id    varchar(2) primary key,
+    value varchar(64) not null
+);
 
-CREATE TABLE IF NOT EXISTS author (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE REFERENCES principal(username) ON DELETE CASCADE ON UPDATE CASCADE
-    );
+create table if not exists user_table
+(
+    id             uuid default gen_random_uuid() primary key,
+    username       varchar(100)     not null unique,
+    password       varchar(40)      not null,
+    age            int              not null check (age > 0),
+    gender         gender_t         not null,
+    country        varchar(2),
+    marital_status marital_status_t not null,
+    constraint country_fk foreign key (country) references country (id)
+        on delete set null
+        on update cascade
+);
 
-CREATE TABLE IF NOT EXISTS country (
-    id VARCHAR(2) PRIMARY KEY,
-    value VARCHAR(64) NOT NULL
-    );
+create table if not exists questionnaire
+(
+    id        uuid default gen_random_uuid() primary key,
+    name      varchar(100) not null,
+    question  varchar(300) not null,
+    author_id uuid,
+    multiple  boolean      not null,
+    constraint author_fk foreign key (author_id) references author (id)
+        on delete set null
+        on update cascade
+);
 
-CREATE TABLE IF NOT EXISTS interviewee (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE REFERENCES principal(username) ON DELETE CASCADE ON UPDATE CASCADE,
-    age INT NOT NULL CHECK (age > 0),
-    gender gender_t NOT NULL,
-    country VARCHAR(2),
-    marital_status marital_status_t NOT NULL,
-    CONSTRAINT country_fk FOREIGN KEY (country) REFERENCES country(id)
-                                                                         ON DELETE SET NULL
-                                                                         ON UPDATE CASCADE
-    );
+create table if not exists answered
+(
+    user_id             uuid not null,
+    questionnaire_id uuid not null,
+    constraint answered_complex_pk primary key (user_id, questionnaire_id),
+    constraint user_fk foreign key (user_id) references user_table(id)
+        on delete cascade
+        on update cascade,
+    constraint questionnaire_fk foreign key (questionnaire_id) references questionnaire(id)
+        on delete cascade
+        on update cascade
+);
 
-CREATE TABLE IF NOT EXISTS questionnaire (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    question VARCHAR(300) NOT NULL,
-    author_id UUID,
-    multiple BOOLEAN NOT NULL,
-    CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES author(id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-    );
+create table if not exists option
+(
+    id               uuid default gen_random_uuid() primary key,
+    text             varchar(75) not null,
+    questionnaire_id uuid        not null,
+    constraint questionnaire_fk foreign key (questionnaire_id) references questionnaire (id)
+        on delete cascade
+        on update cascade
+);
 
-CREATE TABLE IF NOT EXISTS option (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    text VARCHAR(75) NOT NULL,
-    questionnaire_id UUID NOT NULL,
-    CONSTRAINT questionnaire_fk FOREIGN KEY (questionnaire_id) REFERENCES questionnaire(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-    );
-
-CREATE TABLE IF NOT EXISTS choice (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    interviewee_id UUID NOT NULL REFERENCES interviewee(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    questionnaire_id UUID NOT NULL REFERENCES questionnaire(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT unique_interviewee_questionnaire UNIQUE (interviewee_id, questionnaire_id)
-    );
-
-CREATE TABLE IF NOT EXISTS option_choice (
-    option_id UUID NOT NULL REFERENCES option(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    choice_id UUID NOT NULL REFERENCES choice(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT option_choice_pk PRIMARY KEY (option_id, choice_id)
-    );
+create table if not exists choice
+(
+    id             uuid default gen_random_uuid() primary key,
+    option_id      uuid             not null,
+    age            int              not null check (age > 0),
+    gender         gender_t         not null,
+    country        varchar(2),
+    marital_status marital_status_t not null,
+    constraint option_fk foreign key (option_id) references option (id)
+        on delete cascade
+        on update cascade,
+    constraint country_fk foreign key (country) references country (id)
+        on delete set null
+        on update cascade
+);
