6c865d2b23c1254e394aaafdb4aefc912736948e
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-11-02 22:43:25 +0300

    fix: add country service to uses in User mapper to map country objects

--- src/main/java/edu/vsu/putinpa/questionnairesystem/mapper/UserMapper.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/mapper/UserMapper.java
@@ -2,11 +2,11 @@ package edu.vsu.putinpa.questionnairesystem.mapper;
 
 import edu.vsu.putinpa.questionnairesystem.api.dto.request.RegistrationDTO;
 import edu.vsu.putinpa.questionnairesystem.model.User;
+import edu.vsu.putinpa.questionnairesystem.service.CountryService;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface UserMapper {
-    User toUser(RegistrationDTO registrationDTO);
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CountryService.class)
+public abstract class UserMapper {
+    public abstract User toUser(RegistrationDTO registrationDTO);
 }
