6d25c34f2d50549be42b5fbae50eefb769d8c7ef
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-11-02 22:57:15 +0300

    fix: change Controller to RestController in countries controller

--- src/main/java/edu/vsu/putinpa/questionnairesystem/app/controller/CountryController.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/app/controller/CountryController.java
@@ -9,10 +9,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/country")
 @AllArgsConstructor
 public class CountryController implements CountryApi {
