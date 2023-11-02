851a07770b842b2512cec7a8da4d1927898ac9ad
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-11-02 22:11:12 +0300

    fix: change mapper configuration to mapper component model

--- src/main/java/edu/vsu/putinpa/questionnairesystem/mapper/UserMapper.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/mapper/UserMapper.java
@@ -3,10 +3,10 @@ package edu.vsu.putinpa.questionnairesystem.mapper;
 import edu.vsu.putinpa.questionnairesystem.api.dto.request.RegistrationDTO;
 import edu.vsu.putinpa.questionnairesystem.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
     User toUser(RegistrationDTO registrationDTO);
 }
