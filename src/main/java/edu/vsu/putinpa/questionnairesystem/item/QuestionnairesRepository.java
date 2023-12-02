e9ca9309937524348820320eb649abdbeb5cee03
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-02 21:55:53 +0300

    feat: add nameContains method for questionnaire specification

--- src/main/java/edu/vsu/putinpa/questionnairesystem/item/QuestionnairesRepository.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/item/QuestionnairesRepository.java
@@ -1,13 +1,21 @@
 package edu.vsu.putinpa.questionnairesystem.item;
 
 import edu.vsu.putinpa.questionnairesystem.item.model.Questionnaire;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface QuestionnairesRepository extends JpaRepository<Questionnaire, UUID> {
+public interface QuestionnairesRepository extends
+        JpaRepository<Questionnaire, UUID>,
+        JpaSpecificationExecutor<Questionnaire> {
     Optional<Questionnaire> findByName(String name);
 
     void deleteByName(String name);
+
+    static Specification<Questionnaire> nameContains(String name) {
+        return (questionnaire, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(questionnaire.get("name"), "%" + name + "%");
+    }
 }
