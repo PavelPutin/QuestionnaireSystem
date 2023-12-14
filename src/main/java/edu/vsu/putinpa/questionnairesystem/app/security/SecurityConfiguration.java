8ab9fc3150667533175112b0a52f46492a9c26d2
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-14 16:28:53 +0300

    feat: update spring security cofiguration

--- src/main/java/edu/vsu/putinpa/questionnairesystem/app/security/SecurityConfiguration.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/app/security/SecurityConfiguration.java
@@ -19,7 +19,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/registration", "/auth/login", "/questionnaire", "/country").permitAll()
+                        .requestMatchers("/auth/registration", "/auth/login", "/questionnaire", "/questionnaire/popular", "/country").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(login -> login
                         .loginProcessingUrl("/auth/login")
