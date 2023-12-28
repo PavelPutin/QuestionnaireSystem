11a2cc28fbb9e6e683fbe6a2d1402acea628e80d
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-28 23:12:22 +0300

    feat: logger work in register and login methods

--- src/main/java/edu/vsu/putinpa/questionnairesystem/app/controller/AuthController.java
+++ src/main/java/edu/vsu/putinpa/questionnairesystem/app/controller/AuthController.java
@@ -12,6 +12,8 @@ import edu.vsu.putinpa.questionnairesystem.item.model.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.validation.Errors;
@@ -22,14 +24,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
-
 public class AuthController implements AuthApi {
+    private static Logger logger = LogManager.getLogger(AuthController.class);
+
     private final RegistrationService registrationService;
     private final UserMapper userMapper;
     private final SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
 
     @Override
     public void register(RegistrationDTO registrationDTO, Errors errors) {
+        logger.info("Get request to registration with data " + registrationDTO);
         if (errors.hasErrors()) {
             throw new ValidationException(errors);
         }
@@ -39,7 +43,7 @@ public class AuthController implements AuthApi {
 
     @Override
     public UserDTO login(LoginDto loginDto, Errors errors) {
-        System.out.println(loginDto);
+        logger.info("Get request to login with data " + loginDto);
         if (errors.hasErrors()) {
             throw new ValidationException(errors);
         }
